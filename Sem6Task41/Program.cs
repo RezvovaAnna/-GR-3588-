b67bbcd26495e6eab319cdd437c1c0de66fa5178
Anna_Rezvova <rezvova.a@mail.ru>
2022-12-23 20:59:23 +0500

    Sem6Task39, Sem6Task40, Sem6Task41, Sem6Task42, Sem6Task43, Sem6Task44

---
+++ Sem6Task41/Program.cs
@@ -0,0 +1,31 @@
+﻿// Метод вывода результата на экран
+void PrintData(string res, int value)
+{
+    Console.WriteLine(res + value);
+}
+// Метод считывания данных
+int ReadData(string msg)
+{
+    Console.WriteLine(msg);
+    return int.Parse(Console.ReadLine() ?? "0");
+}
+// Метод поиска положительных чисел
+int CalPositivNum (int num)
+{
+  int res = 0;
+  while  (num >0)
+  {
+    if (ReadData("Введите число: ") > 0)
+    {
+      res++ ;
+    }
+
+    num = num - 1;
+  }
+  return res;
+}
+
+int inputNumber = ReadData ("Введите общее количество чисел: ");
+int result = CalPositivNum(inputNumber);
+PrintData("Кoличество чисел больше нуля: ", result);
+
---
+++ Sem6Task43/Program.cs
@@ -0,0 +1,35 @@
+﻿//Метод вывода результата
+void PrintData(string msg, string res)
+{
+    Console.WriteLine(msg+res);
+}
+//Метод чтения данных 
+double ReadData(string msg)
+{
+  Console.Write(msg);
+  return double.Parse(Console.ReadLine() ?? "0");
+}
+//Метод поиска пересечения в координате X
+string SearchCross(double b1, double b2, double k1, double k2)
+{ string res = string.Empty;
+  double resX = (b2 - b1)/(k1-k2);
+  double resY = (k2*b1-k1*b2)/(k2-k1);
+  return res = $"{resX}; {resY}";
+}
+// Считываем переменные
+double numK1=ReadData("Введите K1: ");
+double numK2=ReadData("Введите K2: ");
+double numB1=ReadData("Введите В1: ");
+double numB2=ReadData("Введите B2: ");
+// Проверяем не параллельны ли прямые
+  if(numK1==numK2) 
+{
+ Console.WriteLine("Прямые не пересекаются");
+}
+  else
+{ // Ищем точку пересечения и выводим результат
+string result = SearchCross(numB1, numB2, numK1, numK2);
+PrintData ("Пересечение: ", result);
+}
+
+
