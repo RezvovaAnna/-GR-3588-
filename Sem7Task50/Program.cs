0072fd0fe3dd5b1783c7458e82d1f2486bee9fc9
Anna_Rezvova <rezvova.a@mail.ru>
2022-12-26 19:46:38 +0500

    Sem7Task46, Sem7Task47, Sem7Task48, Sem7Task49, Sem7Task50, Sem7Task51, Sem7Task52 added

---
+++ Sem7Task50/Program.cs
@@ -0,0 +1,72 @@
+﻿//Метод чтения данных 
+int ReadData(string line)
+{
+    Console.Write(line);
+    int number = int.Parse(Console.ReadLine() ?? "0");
+    return number;
+}
+// Метод вывода данных
+void PrintData(string msg, int result)
+{
+    Console.WriteLine(msg + result);
+}
+
+// Универсальный метод генерации и заполнение двумерного массива
+int[,] Fill2DArray(int countRow, int countColumn, int topBorder, int downBorder)
+{
+    System.Random rand = new System.Random();
+    int[,] array2D = new int [countRow, countColumn];
+
+    for (int i = 0; i < countRow; i++)
+    {
+        for (int j = 0; j < countColumn; j++)
+        {
+            array2D[i, j] = rand.Next(topBorder, downBorder + 1);
+        }
+    }
+    return array2D;
+}
+// Метод поиска значаения в двумерно массиве
+int SearchNum (int[,] matrix, int row, int column)
+{    int result = -1;
+     if (row < matrix.GetLength(0))
+     {
+        if(column < matrix.GetLength(1))
+       
+        {
+          result = matrix[row,column];
+        }
+      }
+    return result;
+}
+// Метод вывода массива на экран    
+void Print2DArray(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            
+         Console.Write(matrix[i, j] + "     ".Substring(matrix[i, j].ToString().Length));
+         
+        }
+        Console.WriteLine();
+    }
+}
+
+int row = ReadData("Введите количество строк ");
+int column = ReadData("Введите количество столбцов ");
+int[,] arr2D = Fill2DArray(row, column, 10, 99);
+Print2DArray(arr2D);
+int row1 = ReadData("Введите стороку: ");
+int column1 = ReadData("Введите столбец: ");
+int result = SearchNum (arr2D, row1, column1);
+if (result > -1)
+{
+   PrintData("Ваше число: ", result);
+}
+else 
+{
+    Console.WriteLine("Нет такой позиции");
+}
+
---
+++ Sem7Task52/Program.cs
@@ -0,0 +1,63 @@
+﻿//Метод чтения данных
+int ReadData(string line)
+{
+    Console.Write(line);
+    int number = int.Parse(Console.ReadLine() ?? "0");
+    return number;
+}
+
+// Универсальный метод генерации и заполнение двумерного массива
+double[,] Fill2DArray(int countRow, int countColumn, int topBorder, int downBorder)
+{
+    System.Random rand = new System.Random();
+    double[,] array2D = new double [countRow, countColumn];
+
+    for (int i = 0; i < countRow; i++)
+    {
+        for (int j = 0; j < countColumn; j++)
+        {
+            array2D[i, j] = rand.Next(topBorder, downBorder + 1);
+        }
+    }
+    return array2D;
+}
+// Метод вывода массива на экран
+void Print2DArray( double[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            
+         Console.Write(matrix[i, j] + "     ".Substring(matrix[i, j].ToString().Length));
+         
+        }
+        Console.WriteLine();
+    }
+}
+// Метод поиска среднего арифметического в массиве
+string AverageCalсulator (double [,] arr)
+{   string average = string.Empty; 
+    double averageSum = 0;
+  
+     for ( int j = 0; j<arr.GetLength(0); j++)
+  
+    {    double sum = 0;
+         for (int k = 0; k < arr.GetLength(1); k++)     
+         {
+         sum = sum + arr [k,j];
+         
+         }
+    averageSum = sum/arr.GetLength(1);
+    average = average + averageSum + " ";
+    }
+    
+   return average;
+}
+
+int row = ReadData("Введите количество строк ");
+int column = ReadData("Введите количество столбцов ");
+double [,] arr2D = Fill2DArray(row, column, 0, 10);
+Print2DArray (arr2D);
+string result = AverageCalсulator(arr2D);
+Console.WriteLine (result);
